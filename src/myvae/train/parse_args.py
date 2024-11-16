3e03f32269c0e1891e1f14c8b25d915db2fb1f36
hnmr <hnmr293@gmail.com>
2024-11-17 01:49:50 +0900

    add comments

--- src/myvae/train/parse_args.py
+++ src/myvae/train/parse_args.py
@@ -9,17 +9,42 @@ import myvae
 @dataclass(kw_only=True)  # non-default argument follows default argument
 class TrainConfParams:
     n_epochs: int = 1000
+    """学習エポック数"""
+        
     batch_size: int = 32
+    """バッチサイズ"""
+    
     grad_acc_steps: int = 1
+    """gradient accumulation を行うステップ数"""
+    
     use_gradient_checkpointing: bool = False
+    """gradient checkpointing (activation checkpointing) を行うか"""
+    
     log_freq: int = 50
+    """wandb でのログ頻度（ステップ単位）"""
+    
     pretrained_weight: str|None = None
+    """事前学習したモデルの重み"""
+    
     optimizer: Callable[[Iterable], torch.optim.Optimizer] #= field(default_factory=partial(torch.optim.AdamW, lr=1e-4))
+    """オプティマイザの設定"""
+    
     scheduler: Callable[[torch.optim.Optimizer], torch.optim.lr_scheduler.LRScheduler] #= field(default_factory=partial(torch.optim.lr_scheduler.ConstantLR, factor=1.0, total_iters=0))
+    """学習率スケジューラの設定"""
+    
     save_every_n_epochs: int = 1
-    loss: list[dict]|None = None
+    """モデルの保存頻度"""
+    
     hf_repo_id: str|None = None
+    """モデルの保存先　未指定の場合ローカルに保存される"""
+    
+    loss: list[dict]|None = None
+    """loss の指定
+    
+    {type: str, [weight: float = 1.0], [start_step: int = 0]} の形式"""
+    
     matmul_precision: str = 'highest'
+    """torch.set_float32_matmul_precision に渡す引数"""
 
 
 @dataclass #(frozen=True)  # cannot inferit frozon dataclass from a non-frozen one
@@ -75,7 +100,7 @@ def init_args(p, cfg):
     
     dataloader_class = init.dataloader
     init.dataloader = dataloader_class(init.dataset, init.train.batch_size)
-    init.val_dataloader = dataloader_class(init.val_dataset, 1)
+    init.val_dataloader = dataloader_class(init.val_dataset, 1)  # 何かあると怖いのでバリデーション時のバッチサイズは1にしておく
     init.train.optimizer = init.train.optimizer(init.model.parameters())
     init.train.scheduler = init.train.scheduler(init.train.optimizer)
     
